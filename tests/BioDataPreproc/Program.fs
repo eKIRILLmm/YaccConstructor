4d74c88f3876e0d93b91fb605e12741d533146b6
Kirill <ekirill.mm@gmail.com>
2017-05-10 11:58:59 +0300

    BioDataPreproc

---
+++ tests/BioDataPreproc/Program.fs
@@ -0,0 +1,199 @@
+﻿open VDS.RDF
+open VDS.RDF.Parsing
+
+open System.IO
+open System.Collections.Generic
+
+let loadFromFile (file:string) =
+    let g = new Graph()
+    if (System.IO.Path.GetExtension file).ToLower() = "ttl"
+    then        
+        let ttlparser = new TurtleParser()
+        ttlparser.Load(g, file)
+    else
+        FileLoader.Load(g, file)       
+    g
+
+let writeTriplesFromHomologene file (sw:StreamWriter) =
+    let lines = File.ReadAllLines(file)
+    
+    for l in lines do
+        let elems = l.Split('\t')
+        let homoloGeneGroup = "HomoloGene_" + elems.[0] 
+        let gene = "Gene_" + elems.[2]
+        sw.WriteLine(gene + "\t" + "is_homologous_to" + "\t" + homoloGeneGroup)
+        sw.WriteLine(homoloGeneGroup + "\t" + "-is_homologous_to" + "\t" + gene)
+    printfn "Homologene processed"
+    
+let writeTriplesFromKegg mapFile keggFile (sw:StreamWriter) =
+    let keggLines = File.ReadAllLines(keggFile)
+    for l in keggLines do
+        let elems = l.Split('\t')
+        let pathw = "Pathway_" + elems.[0]
+        let namePath = elems.[1]
+        sw.WriteLine(pathw + "\t" + "name_pathway" + "\t" + namePath)  
+
+    let mapLines = File.ReadAllLines(mapFile)
+    for l in mapLines do
+        let elems = l.Split('\t')
+        let gene = "Gene_" + elems.[0]
+        let pathw = "Pathway_" + elems.[1]
+        sw.WriteLine(gene + "\t" + "has_Pathway" + "\t" + pathw)
+        sw.WriteLine(pathw + "\t" + "-has_Pathway" + "\t" + gene)
+    printfn "KEGG processed"
+
+let writeTriplesFromSTRING mapFile stringFiles (sw:StreamWriter) =
+    let mapLines = File.ReadAllLines(mapFile)
+    let dict = new Dictionary<string, string>()
+    for l in mapLines do
+        let elems = l.Split('\t')
+        if not(dict.ContainsKey elems.[1]) then
+            dict.Add(elems.[1], elems.[0])
+    for f in stringFiles do
+        let interacts = File.ReadAllLines(f)
+        for i in interacts do
+            let elems = i.Split(' ')
+            let prot1 = dict.TryGetValue  elems.[0]
+            let prot2 = dict.TryGetValue  elems.[1]
+            match prot1, prot2 with
+            | (true, p1),(true, p2) -> 
+                sw.WriteLine("Protein_" + p1 + "\t" + "interacts_with" + "\t" + "Protein_" + p2)
+                sw.WriteLine("Protein_" + p2 + "\t" + "interacts_with" + "\t" + "Protein_" + p1)
+            | _ -> ()
+    printfn "STRING processed"
+
+let writeTriplesFromInterpro (file:string) (sw:StreamWriter) =
+    let settings = new System.Xml.XmlReaderSettings()
+    settings.DtdProcessing <- System.Xml.DtdProcessing.Parse
+    let reader = System.Xml.XmlReader.Create(file, settings)
+    while reader.Read()
+        do
+            if reader.Name.Equals "interpro" && reader.IsStartElement()
+            then 
+                let interproId = reader.GetAttribute(0)
+                let proteinCount = reader.GetAttribute(1)
+                let shortName = reader.GetAttribute(2)
+                let interproType = reader.GetAttribute(3)
+                reader.Read() |> ignore
+                reader.Read() |> ignore
+                let name = reader.ReadString()
+                sw.WriteLine(interproId + "\t" + "protein_count" + "\t" + proteinCount)
+                sw.WriteLine(interproId + "\t" + "short_name" + "\t" + shortName)
+                sw.WriteLine(interproId + "\t" + "type" + "\t" + interproType)
+                sw.WriteLine(interproId + "\t" + "name" + "\t" + name)
+    printfn "Interpro processed"
+
+let writeTriplesFromEntrezGene file (sw:StreamWriter) =
+    let lines = File.ReadAllLines(file)
+    for i = 1 to lines.Length - 1 do
+        let elems = lines.[i].Split('\t')
+
+        let tax_id = elems.[0]
+        let Gene = "Gene_" + elems.[1]
+        let Symbol = elems.[2]
+        let LocusTag = elems.[3]
+        let Synonyms = elems.[4]
+        let dbXrefs = elems.[5]
+        let chromosome = elems.[6]
+        let map_location = elems.[7]
+        let description = elems.[8]
+        let type_of_gene = elems.[9]
+        let Symbol_from_nomenclature_authority = elems.[10]
+        let Full_name_from_nomenclature_authority = elems.[11]
+        let Nomenclature_status = elems.[12]
+        let Other_designations = elems.[13]
+        let Modification_date = elems.[14]
+
+        sw.WriteLine(Gene + "\t" + "tax_id" + "\t" + tax_id)
+        sw.WriteLine(Gene + "\t" + "Symbol" + "\t" + Symbol)
+        sw.WriteLine(Gene + "\t" + "LocusTag" + "\t" + LocusTag)
+        sw.WriteLine(Gene + "\t" + "Synonyms" + "\t" + Synonyms)
+        sw.WriteLine(Gene + "\t" + "dbXrefs" + "\t" + dbXrefs)
+        sw.WriteLine(Gene + "\t" + "chromosome" + "\t" + chromosome)
+        sw.WriteLine(Gene + "\t" + "description" + "\t" + description)
+        sw.WriteLine(Gene + "\t" + "type_of_gene" + "\t" + type_of_gene)
+        sw.WriteLine(Gene + "\t" + "map_location" + "\t" + map_location)
+        sw.WriteLine(Gene + "\t" + "Symbol_from_nomenclature_authority" + "\t" + Symbol_from_nomenclature_authority)
+        sw.WriteLine(Gene + "\t" + "Full_name_from_nomenclature_authority" + "\t" + Full_name_from_nomenclature_authority)
+        sw.WriteLine(Gene + "\t" + "Nomenclature_status" + "\t" + Nomenclature_status)
+        sw.WriteLine(Gene + "\t" + "Other_designations" + "\t" + Other_designations)
+        sw.WriteLine(Gene + "\t" + "Modification_date" + "\t" + Modification_date)
+            
+    printfn "EntrezGene processed"
+
+let writeTriplesFromGO file (sw:StreamWriter) =
+    let g = loadFromFile file
+
+    let edg (f: VDS.RDF.INode) (t: VDS.RDF.INode) (l: VDS.RDF.INode) = 
+        match f, t, l with
+        | f, t, l when f.ToString().StartsWith "http://purl.obolibrary.org/obo" ->
+            let GOid = (f :?> UriNode).Uri.Segments.[(f :?> UriNode).Uri.Segments.Length - 1]
+            sw.WriteLine(GOid + "\t" + l.ToString() + "\t" + t.ToString())            
+        | _ -> 
+            sw.WriteLine(f.ToString() + "\t" + l.ToString() + "\t" + t.ToString())  
+            
+    for t in g.Triples do
+        edg t.Object t.Subject t.Predicate
+
+    printfn "GO processed"
+
+let writeTriplesFromUniprot file (sw:StreamWriter) =
+    let g = loadFromFile file
+    let triples = g.Triples.Count
+
+    let mutable curProt = ""
+    let mutable curGene = ""
+    let edg (f: VDS.RDF.INode) (t: VDS.RDF.INode) (l: VDS.RDF.INode) = 
+        match f, t, l with
+        | f, t, l when f.ToString() = "http://purl.uniprot.org/core/Protein" -> 
+            curProt <- "Protein_" + (t :?> UriNode).Uri.Segments.[(t :?> UriNode).Uri.Segments.Length - 1]
+            //printfn "%A, %A, %A" (f.ToString()) (l.ToString()) curProt
+            sw.WriteLine(f.ToString() + "\t" + l.ToString() + "\t" + curProt)
+
+        | f, t, l when f.ToString().StartsWith "http://purl.uniprot.org/interpro" -> 
+            let interpro = "FamOrDom_" + (f :?> UriNode).Uri.Segments.[(f :?> UriNode).Uri.Segments.Length - 1]
+            //printfn "%A, %A, %A" curProt "has_FamOrDom" interpro
+            sw.WriteLine(curProt + "\t" + "has_FamOrDom" + "\t" + interpro)
+            sw.WriteLine(interpro + "\t" + "-has_FamOrDom" + "\t" + curProt)
+            
+        | f, t, l when l.ToString() = "http://purl.uniprot.org/core/classifiedWith" && (f :?> UriNode).Uri.Segments.[(f :?> UriNode).Uri.Segments.Length - 1].StartsWith("GO") ->
+            let GOid = (f :?> UriNode).Uri.Segments.[(f :?> UriNode).Uri.Segments.Length - 1]
+            //printfn "%A, %A, %A" curProt "belongs_to" GOid
+            sw.WriteLine(curProt + "\t" + "belongs_to" + "\t" + GOid)
+            sw.WriteLine(GOid + "\t" + "-belongs_to" + "\t" + curProt)
+
+        | f, t, l when f.ToString() = "http://purl.uniprot.org/database/GeneID" ->
+            let gene = "Gene_" + (t :?> UriNode).Uri.Segments.[(t :?> UriNode).Uri.Segments.Length - 1]
+            curGene <- gene
+            //printfn "%A, %A, %A" gene "codes_for" curProt
+            sw.WriteLine(gene + "\t" + "codes_for" + "\t" + curProt)
+            sw.WriteLine(curProt + "\t" + "-codes_for" + "\t" + gene)
+
+        | f, t, l when f.ToString() = "phenotype" -> 
+            let MIMid = "Phenotype_" + (t :?> UriNode).Uri.Segments.[(t :?> UriNode).Uri.Segments.Length - 1]
+            //printfn "%A, %A, %A" fId MIMid tId
+            sw.WriteLine(curGene + "\t" + "refers_to" + "\t" + MIMid)
+            sw.WriteLine(MIMid + "\t" + "-refers_to" + "\t" + curGene)
+
+        | _ ->
+            sw.WriteLine(f.ToString() + "\t" + l.ToString() + "\t" + t.ToString())               
+    
+    for t in g.Triples do
+        edg t.Object t.Subject t.Predicate
+    printfn "Uniprot processed"
+
+let writeAllTriples basePath (sw:StreamWriter) =
+    for f in System.IO.Directory.GetFiles (basePath + "\UNIPROT") do writeTriplesFromUniprot f sw
+    for f in System.IO.Directory.GetFiles (basePath + "\Entrez Gene") do writeTriplesFromEntrezGene f sw
+    writeTriplesFromSTRING (basePath + "\map\UniprotToString.txt") (System.IO.Directory.GetFiles (basePath + "\STRING")) sw
+    writeTriplesFromKegg (basePath + "\map\geneToPath.txt") (basePath + "\KEGG\pathways.keg") sw
+    writeTriplesFromInterpro (basePath + "\InterPro\interpro.xml") sw
+    writeTriplesFromGO (basePath + "\Gene Ontology\go.owl") sw
+    writeTriplesFromHomologene (basePath + "\HomoloGene\homologene.data.txt") sw
+    
+[<EntryPoint>]
+let main argv = 
+    let basePath = @"..\..\..\data\BioData"
+    let sw = new StreamWriter(@"..\..\..\data\BioData\result\allTriples.txt")
+    writeAllTriples basePath sw
+    0
--- tests/GLL.AbstractParser.Simple.Tests/BioDataPerformance.fs
+++ tests/GLL.AbstractParser.Simple.Tests/BioDataPerformance.fs
@@ -36,282 +36,36 @@ let getTokenFromTag tokenizer (tag:string) =
     | Equals "has_FamOrDom" () -> tokenizer "HAS_FAM_OR_DOM"
     | _ -> tokenizer "OTHER"
 
+let getParseInputGraph file =
 
-let loadFromFile (file:string) =
-    let g = new Graph()
-    if (System.IO.Path.GetExtension file).ToLower() = "ttl"
-    then        
-        let ttlparser = new TurtleParser()
-        ttlparser.Load(g, file)
-    else
-        FileLoader.Load(g, file)       
-    g
+    let vMap = new System.Collections.Generic.Dictionary<_,_>()
+    let mutable idV = -1
+    let getId v = if vMap.ContainsKey v then true, vMap.[v] else (idV <- idV + 2; vMap.Add(v, idV); false, idV)
 
-let vMap = new System.Collections.Generic.Dictionary<_,_>()
-let mutable idV = -1
-let getId v = if vMap.ContainsKey v then vMap.[v] else (idV <- idV + 2; vMap.Add(v, idV); idV)
- 
-
-let getEdgesFromHomologene file =
-    let lines = File.ReadAllLines(file)
-    [|
-        for l in lines->
-            let elems = l.Split('\t')
-            let homoloGeneGroup = "HomoloGene_" + elems.[0] 
-            let gene = "Gene_" + elems.[2]
-            [|
-                getId gene + 1, "is_homologous_to", getId homoloGeneGroup;
-                getId homoloGeneGroup, homoloGeneGroup, getId homoloGeneGroup + 1;
-            |]
-    |] |> Array.concat
-
-let getEdgesFromKeggPath file = 
-    let lines = File.ReadAllLines(file)
-    [|
-        for l in lines->
-            let elems = l.Split('\t')
-            let pathw = "Pathway_" + elems.[0]
-            let namePath = "name_pathway" + elems.[1]
-            [|
-                getId pathw, pathw, getId pathw + 1
-                getId pathw + 1, "name path", getId namePath;
-                getId namePath, namePath, getId namePath + 1;
-            |]
-    |] |> Array.concat
-
-let getEdgesFromKeggGeneMap file =
-    let lines = File.ReadAllLines(file)
-    [|
-        for l in lines->
-            let elems = l.Split('\t')
-            let gene = "Gene_" + elems.[0]
-            let pathw = "Pathway_" + elems.[1]
-            [|
-                getId gene + 1, "has_Pathway", getId pathw;
-            |]
-    |] |> Array.concat
-
-let getEdgesFromInterpro file =
-    let lines = File.ReadAllLines(file)
-    [|
-        for l in lines->
-            let elems = l.Split('\t')
-            let ipro = "FamOrDom_" + elems.[0];
-            let protein_count = elems.[1]
-            let short_name = elems.[2]
-            let iproType = elems.[3]
-            let name = elems.[4]
-            let iproId = getId ipro
-            [|
-                iproId, ipro, iproId + 1;
-
-                iproId + 1, "potein_count", getId protein_count;
-                getId protein_count, protein_count, getId protein_count + 1;
-
-                iproId + 1, "iprot_short_name", getId short_name;
-                getId short_name, short_name, getId short_name + 1;
-
-                iproId + 1, "iprot_type", getId iproType;
-                getId iproType, iproType, getId iproType + 1;
-
-                iproId + 1, "iprot_name", getId name;
-                getId name, name, getId name + 1;
-            |]
-    |] |>Array.concat
-    
-
-let getEdgesFromEntrezGene file =
-    let lines = File.ReadAllLines(file)
-    [|
+    let getEdges f = 
+        let lines = File.ReadLines(file)
+        [|
         for l in lines ->
             let elems = l.Split('\t')
+            let f = elems.[0]
+            let lbl = elems.[1]
+            let t = elems.[2]
 
-            let tax_id = elems.[0]
-            let Gene = "Gene_" + elems.[1]
-            let Symbol = elems.[2]
-            let LocusTag = elems.[3]
-            let Synonyms = elems.[4]
-            let dbXrefs = elems.[5]
-            let chromosome = elems.[6]
-            let map_location = elems.[7]
-            let description = elems.[8]
-            let type_of_gene = elems.[9]
-            let Symbol_from_nomenclature_authority = elems.[10]
-            let Full_name_from_nomenclature_authority = elems.[11]
-            let Nomenclature_status = elems.[12]
-            let Other_designations = elems.[13]
-            let Modification_date = elems.[14]
+            match (getId f), (getId t)  with
+            | (false, fId), (false, tId) -> [|fId, f, fId + 1;
+                                              fId + 1, lbl, tId;
+                                              tId, t, tId + 1;|]
+                                            
+            | (true, fId), (false, tId) -> [|fId + 1, lbl, tId;
+                                             tId, t, tId + 1;|]
 
-            let geneId = getId Gene
-            //printfn "%A, %A, %A" geneId Gene (geneId+1)
-            [|
-                geneId, Gene, geneId + 1;
+            | (false, fId), (true, tId) -> [|fId, f, fId + 1;
+                                             fId + 1, lbl, tId;|]
 
-                geneId + 1, "tax_id", getId tax_id;
-                getId tax_id, tax_id,  (getId tax_id) + 1;
+            | (true, fId), (true, tId) -> [|fId + 1, lbl, tId;|]
+        |]
 
-                geneId + 1, "Symbol", getId Symbol;
-                getId Symbol, Symbol,  (getId Symbol) + 1;
-
-                geneId + 1, "LocusTag", getId LocusTag;
-                getId LocusTag, LocusTag,  (getId LocusTag) + 1;
-
-                geneId + 1, "Synonyms", getId Synonyms;
-                getId Synonyms, Synonyms,  (getId Synonyms) + 1;
-
-                geneId + 1, "dbXrefs", getId dbXrefs;
-                getId dbXrefs, dbXrefs,  (getId dbXrefs) + 1;
-
-                geneId + 1, "chromosome", getId chromosome;
-                getId chromosome, chromosome,  (getId chromosome) + 1;
-
-                geneId + 1, "description", getId description;
-                getId description, description,  (getId description) + 1;
-
-                geneId + 1, "type_of_gene", getId type_of_gene;
-                getId type_of_gene, type_of_gene,  (getId type_of_gene) + 1;
-
-                geneId + 1, "map_location", getId map_location;
-                getId map_location, map_location,  (getId map_location) + 1;
-
-                geneId + 1, "Symbol_from_nomenclature_authority", getId Symbol_from_nomenclature_authority;
-                getId Symbol_from_nomenclature_authority, Symbol_from_nomenclature_authority,  (getId Symbol_from_nomenclature_authority) + 1;
-
-                geneId + 1, "Full_name_from_nomenclature_authority", getId Full_name_from_nomenclature_authority;
-                getId Full_name_from_nomenclature_authority, Full_name_from_nomenclature_authority,  (getId Full_name_from_nomenclature_authority) + 1;
-
-                geneId + 1, "Nomenclature_status", getId Nomenclature_status;
-                getId Nomenclature_status, Nomenclature_status,  (getId Nomenclature_status) + 1;
-
-                geneId + 1, "Other_designations", getId Other_designations;
-                getId Other_designations, Other_designations,  (getId Other_designations) + 1;
-
-                geneId + 1, "Modification_date", getId Modification_date;
-                getId Modification_date, Modification_date,  (getId Modification_date) + 1;
-
-            |]
-    |] |> Array.concat
-    
-
-let getEdgesFromGO file =
-    let g = loadFromFile file
-
-    let edg (f: VDS.RDF.INode) (t: VDS.RDF.INode) (l: VDS.RDF.INode) = 
-        match f, t, l with
-        | f, t, l when f.ToString().StartsWith "http://purl.obolibrary.org/obo" ->
-            let GOid = (f :?> UriNode).Uri.Segments.[(f :?> UriNode).Uri.Segments.Length - 1]
-            [|
-                getId GOid, GOid, getId GOid + 1;
-                getId GOid + 1, l.ToString(), getId (t.ToString());
-                getId (t.ToString()), t.ToString(), getId (t.ToString()) + 1;
-            |]
-        | _ -> 
-            let fId = getId (f.ToString())
-            let tId = getId (t.ToString())
-            [|fId, f.ToString(), fId + 1;
-            fId + 1, l.ToString(), tId;
-            tId, t.ToString(), tId + 1|]
-
-    let edgs = [|for t in g.Triples -> edg t.Object t.Subject t.Predicate |] |> Array.concat
-    edgs
-
-let getEdgesFromUniprot file =
-    let g = loadFromFile file
-    let triples = g.Triples.Count
-
-    let mutable curProt = ""
-    let mutable curGene = ""
-    let edg (f: VDS.RDF.INode) (t: VDS.RDF.INode) (l: VDS.RDF.INode) = 
-        match f, t, l with
-        | f, t, l when f.ToString() = "http://purl.uniprot.org/core/Protein" -> 
-            curProt <- "Protein_" + (t :?> UriNode).Uri.Segments.[(t :?> UriNode).Uri.Segments.Length - 1]
-            let fId = getId (f.ToString())
-            let tId = getId (t.ToString())
-            let protId = getId curProt
-            //printfn "%A, %A, %A" protId curProt (protId + 1)
-            [|
-                fId, f.ToString(), fId + 1;
-                fId + 1, l.ToString(), protId;
-                protId, curProt, protId + 1
-            |]
-        | f, t, l when f.ToString().StartsWith "http://purl.uniprot.org/interpro" -> 
-            let interpro = "FamOrDom_" + (f :?> UriNode).Uri.Segments.[(f :?> UriNode).Uri.Segments.Length - 1]
-            let interproID = getId interpro
-            let protId = getId curProt
-            //printfn "%A, %A, %A" fId curProt tId
-            [|
-                protId + 1, "has_FamOrDom", interproID;
-                interproID, interpro, interproID + 1
-            |]
-            
-        | f, t, l when l.ToString() = "http://purl.uniprot.org/core/classifiedWith" && (f :?> UriNode).Uri.Segments.[(f :?> UriNode).Uri.Segments.Length - 1].StartsWith("GO") ->
-            let GOid = (f :?> UriNode).Uri.Segments.[(f :?> UriNode).Uri.Segments.Length - 1]
-            let fId = getId curProt
-            let tId = getId GOid
-            //printfn "%A, %A, %A" fId GOid tId
-
-            [|
-                fId + 1, "belongs_to", tId;
-                tId + 1, "-belongs_to", fId;
-            |]
-
-        | f, t, l when f.ToString() = "http://purl.uniprot.org/database/GeneID" ->
-            let gene = "Gene_" + (t :?> UriNode).Uri.Segments.[(t :?> UriNode).Uri.Segments.Length - 1]
-            curGene <- gene
-            let protId = getId curProt
-            let geneId = getId gene
-            //printfn "%A, %A, %A" geneId gene (geneId+1)
-            [|
-                protId + 1, "-codes_for", geneId;
-                geneId + 1, "codes_for", protId;
-            |]
-
-        | f, t, l when f.ToString() = "phenotype" -> 
-            let MIMid = "Phenotype_" + (t :?> UriNode).Uri.Segments.[(t :?> UriNode).Uri.Segments.Length - 1]
-            let fId = getId curGene
-            let tId = getId MIMid
-            //printfn "%A, %A, %A" fId MIMid tId
-            [|
-                fId + 1, "refers_to", tId;
-                tId, MIMid, tId + 1;
-            |]
-
-        | _ -> let fId = getId (f.ToString())
-               let tId = getId (t.ToString())
-               [|
-                   fId, f.ToString(), fId + 1;
-                   fId + 1, l.ToString(), tId;
-                   tId, t.ToString(), tId + 1
-               |]
-               
-    
-    let edgs = [|for t in g.Triples -> edg t.Object t.Subject t.Predicate |] |> Array.concat
-    edgs
-
-let getParseInputGraph files =
-    let getEdges f = 
-        match System.IO.Path.GetFileName f with
-        | "go.owl" -> getEdgesFromGO f
-        | "uniprot-for_test.rdf" 
-        | "uniprot-homo_sapiens.rdf" 
-        | "uniprot-c_elegans.rdf" 
-        | "uniprot-melanogaster.rdf" 
-        | "uniprot-mus_musculus.rdf" 
-        | "uniprot-rattus_norvegicus.rdf" -> getEdgesFromUniprot f
-        | "Caenorhabditis_elegans.gene_info"
-        | "Drosophila_melanogaster.gene_info"
-        | "Mus_musculus.gene_info"
-        | "Rattus_norvegicus.gene_info"
-        | "Homo_sapiens.gene_info" -> getEdgesFromEntrezGene f
-        | "interpro.txt" -> getEdgesFromInterpro f
-        | "pathways.keg" -> getEdgesFromKeggPath f
-        | "geneToPath.txt" -> getEdgesFromKeggGeneMap f
-        | "homologene.data.txt" -> getEdgesFromHomologene f
-        | _ -> [||]
-
-    let edgs = files 
-            |> Array.map getEdges 
-            |> Array.concat 
+    let edgs = getEdges file |> Array.concat 
     
 //    let edgs = 
 //        [|
@@ -337,7 +91,7 @@ let getParseInputGraph files =
     let allVs = edgs |> Array.collect (fun (f,l,t) -> [|f * 1<positionInInput>; t * 1<positionInInput>|]) |> Set.ofArray |> Array.ofSeq
     let eofV = allVs.Length
         
-    let graph = new SimpleInputGraph<_>([|13863 * 1<positionInInput>|], getTokenFromTag (fun x -> (int) GLL.GPPerf1.stringToToken.[x]))
+    let graph = new SimpleInputGraph<_>([|1 * 1<positionInInput>|], getTokenFromTag (fun x -> (int) GLL.GPPerf1.stringToToken.[x]))
     
     edgs
     |> Array.collect (fun (f,l,t) -> [|new ParserEdge<_>(f, t, l)|])
@@ -348,29 +102,25 @@ let getParseInputGraph files =
 
     graph, graph.EdgeCount
         
-let processFiles files =
+let processFile file =
     let g1, edges = 
-        getParseInputGraph files 
-//    for e in g1.Edges do
-//        printfn "%A; %A" (e.Tag) (e.ToString())
-    let cnt = 1
+        getParseInputGraph file 
+
     let start = System.DateTime.Now
     printfn "%A" edges
     let root1 =
         Yard.Generators.GLL.AbstractParser.getAllSPPFRoots GLL.GPPerf1.parserSource g1
-    root1.[0].AstToDot GLL.GPPerf1.intToString "qwe.dot"
+//    root1.[0].AstToDot GLL.GPPerf1.intToString "qwe.dot"
 //    let root1 =
 //        Yard.Generators.GLL.AbstractParser.getAllRangesForStartState GLL.GPPerf1.parserSource g1
 //        |> Seq.length
 
-    let time1 = (System.DateTime.Now - start).TotalMilliseconds / (float cnt)
+    let time1 = (System.DateTime.Now - start).TotalMilliseconds
 
     printfn "roots %A" root1.Length
     edges, time1, root1
 
 let performTests () =
-    let basePath = @"..\..\..\data\BioData"
-    let files = System.IO.Directory.GetFiles basePath    
-    files 
-    |> processFiles
+    let allTriplesFile = @"..\..\..\data\BioData\result\allTriples.txt"    
+    processFile allTriplesFile
     |> printfn "%A"
